0b10096ffbd4695b23903a316da583a0ee40318c
unknown <nadavlevy40@gmail.com>
2022-11-02 17:52:21 +0200

    remove list from home routre

--- app.js
+++ app.js
@@ -42,43 +42,43 @@ const List=mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
 
-   Item.find({},function(err,foundItems){
-    // List.findOne({name:"Today"},function(err,foundList) {
-    //   if(!err)
-    //   {
-    //     if(!foundList)
-    //     {
-    //         const list=new List({
-    //           name:customList,
-    //           items:defaultItems
-    //         });
-    //         list.save();
-    //         res.redirect("/");
-    //     }
-    //     else{
-    //       res.render("list", {listTitle:"Today", newListItems:foundList.items});
+  //  Item.find({},function(err,foundItems){
+  //   // List.findOne({name:"Today"},function(err,foundList) {
+  //   //   if(!err)
+  //   //   {
+  //   //     if(!foundList)
+  //   //     {
+  //   //         const list=new List({
+  //   //           name:customList,
+  //   //           items:defaultItems
+  //   //         });
+  //   //         list.save();
+  //   //         res.redirect("/");
+  //   //     }
+  //   //     else{
+  //   //       res.render("list", {listTitle:"Today", newListItems:foundList.items});
+  //   // }
+  //   //   }
+  //   if(foundItems.length==0)
+  //   {
+  //     Item.insertMany(defaultItems,function(err){
+  //       if(err)
+  //       {
+  //          console.log(err);
+  //       }
+  //       else
+  //       {
+  //          console.log("successfuly inseted to document!");
+  //       }
+  //     });
+      //  res.redirect("/");
     // }
-    //   }
-    if(foundItems.length==0)
-    {
-      Item.insertMany(defaultItems,function(err){
-        if(err)
-        {
-           console.log(err);
-        }
-        else
-        {
-           console.log("successfuly inseted to document!");
-        }
-      });
-       res.redirect("/");
-    }
-    else{
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    // else{
+      res.render("list", {listTitle: "Today", newListItems: defaultItems});
 
-    }
+    // }
 
-   });
+  //  });
 });
 
 app.post("/", function(req, res){
